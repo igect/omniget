d7042e71bb4f611dc77063fd0f4f7640f150589b
Copyright News <buyfragments@gmail.com>
2026-07-08 03:15:55 +0600

    feat: update about section links and credits to sayfalse

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -24,7 +24,7 @@
 
     async function openAuthorGithub(e: Event) {
         e.preventDefault();
-        await open("https://github.com/tonhowtf");
+        await open("https://github.com/sayfalse");
     }
 </script>
 
@@ -55,7 +55,7 @@
     </div>
 
     <div class="about-external">
-        <a href="https://github.com/tonhowtf/omniget" target="_blank" rel="noopener" class="about-ext-link">
+        <a href="https://github.com/sayfalse/omniget-mint" target="_blank" rel="noopener" class="about-ext-link">
             <svg viewBox="0 0 24 24" width="16" height="16" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"/>
             </svg>
@@ -70,9 +70,9 @@
     </div>
 
     <footer class="about-footer">
-        <p class="about-credit">{$t("about.credit")}</p>
-        <a href="https://github.com/tonhowtf" class="about-watermark" onclick={openAuthorGithub} title="@tonhowtf">
-            @tonhowtf
+        <p class="about-credit">Made with love by sayfalse</p>
+        <a href="https://github.com/sayfalse" class="about-watermark" onclick={openAuthorGithub} title="@sayfalse">
+            @sayfalse
         </a>
     </footer>
 </div>
