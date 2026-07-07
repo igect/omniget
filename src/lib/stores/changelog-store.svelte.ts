b04c9022d620a8bef2bd2058b345ec8deda156c1
Copyright News <buyfragments@gmail.com>
2026-07-08 04:44:30 +0600

    chore: bump version to 0.7.1

--- src/lib/stores/changelog-store.svelte.ts
+++ src/lib/stores/changelog-store.svelte.ts
@@ -39,7 +39,7 @@ export async function initChangelog(): Promise<void> {
   try {
     currentVersion = await getVersion();
   } catch {
-    currentVersion = "0.7.0";
+    currentVersion = "0.7.1";
   }
 
   const lastSeen = localStorage.getItem(STORAGE_KEY);
--- src/routes/about/project/+page.svelte
+++ src/routes/about/project/+page.svelte
@@ -3,7 +3,7 @@
   import { t } from "$lib/i18n";
 
   const GITHUB_URL = "https://github.com/sayfalse/omniget";
-  const APP_VERSION = "0.7.0";
+  const APP_VERSION = "0.7.1";
 
   async function openGitHub() {
     await open(GITHUB_URL);
